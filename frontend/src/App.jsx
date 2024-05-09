434fc9a0079092256d6673424657021a0487c70e
Thomas Schwallek <thomas.schwallek@icloud.com>
2024-05-09 20:58:03 +0200

    Allgemeine Struktur aktualisiert, Hintergrund und Header eingefügt, Navbar angepasst.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-import Navbar from './component/navbar';
-import Haupt from './component/haupt';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/navbar.jsx';
+import HeaderLogo from './component/headerlogo.jsx';
+import Training from './pages/training.jsx';
+import Calender from './pages/calender';
+import History from './pages/history';
+import Account from './pages/account';
+import './styles.css'
 
-class App extends Component {
-    state = {  } 
-    render() { 
-        return <React.Fragment>
-            <Haupt/>
-        </React.Fragment>;
-    }
+
+function App() {
+    return (
+        <Router>
+            <React.Fragment>
+                <div className='background-container'>
+                </div>
+                <HeaderLogo/>
+                <Navbar />
+                <Routes>
+                    <Route exact path="/pages/training.jsx" component={Training} />
+                    <Route path="/pages/calendar.jsx" component={Calender} />
+                    <Route path="/pages/history.jsx" component={History} />
+                    <Route path="/pages/account.jsx" component={Account} />
+                </Routes>
+            </React.Fragment>
+        </Router>
+    )
 }
- 
+
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import ReactDom from "react-dom/client"
+import ReactDom from "react-dom"
 import App from "./App"
-import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-
-root.render(<App/>);
+ReactDom.render(<App/>, document.getElementById('root'));
