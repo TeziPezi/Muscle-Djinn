49b4be5da9bf3ed677b99dd4671014ec25ddd8f4
Thomas Schwallek <thomas.schwallek@icloud.com>
2024-05-15 17:31:06 +0200

    Login & Register Button auf Homepage zugefügt

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,14 +3,22 @@ import '../styles.css';
 
 
 class Home extends Component {
-    state = {};
+    handleLoginRedirect = () => {
+        window.location.href = 'https://www.example.com/login'; // Ändere die URL zur gewünschten Login-Seite
+    };
+    handleRegisterRedirect = () => {
+        window.location.href = 'https://www.example.com/register'; // Ändere die URL zur gewünschten Register-Seite
+    };
+
     render() {
         return (
             <React.Fragment>
                 <div className="headPosition">
                     <div className="container">
                         The Homepage.<br/><br/>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quisquam blanditiis odit rerum at minima saepe, magnam illum iusto, officia et optio debitis eum quas consequatur dicta minus non illo temporibus perspiciatis harum cumque! Repellendus dolorem, itaque esse ipsum atque similique minus tenetur natus quibusdam. Laboriosam reiciendis perferendis quidem dicta.#
+                        <button type="button" className="Button" onClick={this.handleLoginRedirect}>Login</button>
+                        <br/><br/>
+                        <button type="button" className="Button" onClick={this.handleRegisterRedirect}>Register</button>
                     </div>
                 </div>
             </React.Fragment>
