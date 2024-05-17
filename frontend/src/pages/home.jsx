5ff7fcd9c936a333484ab0307a01231e185f03c5
Thomas Schwallek <thomas.schwallek@icloud.com>
2024-05-17 09:57:00 +0200

    Button Anpassung Login

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink } from "react-router-dom"
 import '../styles.css';
 
 
 class Home extends Component {
-    handleLoginRedirect = () => {
-        window.location.href = './loginForm'; // Ändere die URL zur gewünschten Login-Seite
-    };
-    handleRegisterRedirect = () => {
-        window.location.href = 'https://www.example.com/register'; // Ändere die URL zur gewünschten Register-Seite
-    };
-
     render() {
         return (
             <React.Fragment>
                 <div className="headPosition">
                     <div className="container">
                         The Homepage.<br/><br/>
-                        <button type="button" className="Button" onClick={this.handleLoginRedirect}>Login</button>
+                        <button type="button" className="Button"><NavLink to="/loginForm">Login</NavLink></button>
+                        
                         <br/><br/>
-                        <button type="button" className="Button" onClick={this.handleRegisterRedirect}>Register</button>
+                        <button type="button" className="Button" >Register</button>
                     </div>
                 </div>
             </React.Fragment>
