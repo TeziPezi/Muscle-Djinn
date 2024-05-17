87aa80ce33507d069581be2370646a3ae312a61f
Thomas Schwallek <thomas.schwallek@icloud.com>
2024-05-17 10:28:27 +0200

    Neuer Versuch Login-Button

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import { NavLink } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 import '../styles.css';
 
-
 class Home extends Component {
+    handleLoginClick = () => {
+        this.props.navigate('/loginForm');
+    };
+
     render() {
         return (
             <React.Fragment>
                 <div className="headPosition">
                     <div className="container">
                         The Homepage.<br/><br/>
-                        <button type="button" className="Button"><NavLink to="/loginForm">Login</NavLink></button>
-                        
+                        <button type="button" className="Button" onClick={this.handleLoginClick}>
+                            Login
+                        </button>
                         <br/><br/>
-                        <button type="button" className="Button" >Register</button>
+                        <button type="button" className="Button">Register</button>
                     </div>
                 </div>
             </React.Fragment>
@@ -21,4 +25,10 @@ class Home extends Component {
     }
 }
 
-export default Home;
+// Ein Wrapper, um useNavigate zu nutzen und es als Prop an die Home-Komponente weiterzugeben
+const HomeWithNavigate = (props) => {
+    const navigate = useNavigate();
+    return <Home {...props} navigate={navigate} />;
+};
+
+export default HomeWithNavigate;
