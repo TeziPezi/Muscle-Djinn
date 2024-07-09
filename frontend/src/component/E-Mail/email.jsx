49a3a911813330c8abab33cb40b9c1a0a62b3e0f
Nathitan <Nathitan.Sallach@gmail.com>
2024-07-09 15:39:28 +0200

    EMail Frontend

--- frontend/src/component/E-Mail/email.jsx
+++ frontend/src/component/E-Mail/email.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 function EMail() {
+    const navigate = () => {
+        window.location.href = "/Code";
+    }
     return <React.Fragment>
-        <span style={{ color: 'white' }}><h2>Password zurücksetzen</h2></span>
-        <span style={{ color: 'white' }}>Bitte geben sie ihre E-Mail eine, um ihr Password zurücksetzen</span>
-        <div className="emailsuche">
-            <form action="/Code">
-                <input type="email" name="email" placeholder="E-Mail" />
-            </form>
-            <button type="sumbit">Suchen</button><br/>
+        <div style={{ height: "300px", width: "400px", border: "2px solid white", margin: "0 auto", marginTop: "200px", textAlign: "center"}}>
+            <span style={{ color: "white" }}><h3>Password zurücksetzen</h3></span>
+            <span style={{ color: "white" }}>Bitte geben sie ihre E-Mail eine,</span><br/>
+            <span style={{ color: "white" }}>um ihr Password zurücksetzen</span>
+            <div style={{ margin: "20px" }}>
+                <form>
+                    <input type="email" name="email" placeholder="E-Mail" />
+                </form>
+                <button onClick={navigate} style={{ margin: "20px", height: "50px", width: "200px"}}>Suchen</button><br/>
+            </div>
         </div>
     </React.Fragment>
 }
